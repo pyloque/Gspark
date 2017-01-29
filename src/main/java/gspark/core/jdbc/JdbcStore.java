4b135388d6c0b6592213b5cf71f2b4a0f7a78dd2
pyloque <pyloque@gmail.com>
2017-01-29 16:48:59 +0800

    encapsulate jdbc operations

--- src/main/java/gspark/core/jdbc/JdbcStore.java
+++ src/main/java/gspark/core/jdbc/JdbcStore.java
@@ -2,13 +2,19 @@ package gspark.core.jdbc;
 
 import java.beans.PropertyVetoException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
+import gspark.core.Holder;
 import gspark.core.config.JdbcConfig;
 import gspark.core.error.JdbcError;
 
@@ -55,6 +61,87 @@ public class JdbcStore {
 				}
 			}
 		}
+	}
+
+	public void select(String sql, List<Object> params, Consumer<ResultSet> rsConsumer) {
+		this.execute(conn -> {
+			PreparedStatement stmt = null;
+			ResultSet rst = null;
+			try {
+				stmt = conn.prepareStatement(sql);
+				for (int i = 0; i < params.size(); i++) {
+					Object o = params.get(i);
+					stmt.setObject(i, o);
+				}
+				rst = stmt.executeQuery();
+				rsConsumer.accept(rst);
+			} finally {
+				if (rst != null)
+					rst.close();
+				if (stmt != null)
+					stmt.close();
+			}
+		});
+	}
+
+	public int execute(String sql, List<Object> params) {
+		Holder<Integer> holder = new Holder<>();
+		this.execute(conn -> {
+			PreparedStatement stmt = null;
+			try {
+				stmt = conn.prepareStatement(sql);
+				for (int i = 0; i < params.size(); i++) {
+					Object o = params.get(i);
+					stmt.setObject(i, o);
+				}
+				holder.set(stmt.executeUpdate());
+			} finally {
+				if (stmt != null)
+					stmt.close();
+			}
+		});
+		return holder.value();
+	}
+
+	public int[] batch(String sql, List<List<Object>> multiParams) {
+		Holder<int[]> holder = new Holder<>();
+		this.execute(conn -> {
+			PreparedStatement stmt = null;
+			try {
+				stmt = conn.prepareStatement(sql);
+				for (List<Object> params : multiParams) {
+					for (int i = 0; i < params.size(); i++) {
+						stmt.setObject(i, params.get(i));
+					}
+					stmt.addBatch();
+				}
+				holder.set(stmt.executeBatch());
+			} finally {
+				if (stmt != null) {
+					stmt.close();
+				}
+			}
+		});
+		return holder.value();
+	}
+
+	public int[] batch(List<String> sqls) {
+		Holder<int[]> holder = new Holder<>();
+		this.execute(conn -> {
+			Statement stmt = null;
+			try {
+				stmt = conn.createStatement();
+				for (String sql : sqls) {
+					stmt.addBatch(sql);
+				}
+				holder.set(stmt.executeBatch());
+			} finally {
+				if (stmt != null) {
+					stmt.close();
+				}
+			}
+		});
+		return holder.value();
 	}
 
 }
--- src/main/java/gspark/core/view/ApiError.java
+++ src/main/java/gspark/core/view/ApiError.java
@@ -5,7 +5,8 @@ public class ApiError extends RuntimeException {
 	private static final long serialVersionUID = -2486664259162937617L;
 
 	private int code;
-	private final static int CODE_SERVER_ERROR = 1;
+	private final static int CODE_BACKEND_ERROR = 1;
+	private final static int CODE_UNKNOWN_ERROR = 2;
 
 	public ApiError(int code, String msg) {
 		super(msg);
@@ -13,7 +14,11 @@ public class ApiError extends RuntimeException {
 	}
 
 	public static ApiError newServerError(String msg) {
-		return new ApiError(CODE_SERVER_ERROR, msg);
+		return new ApiError(CODE_BACKEND_ERROR, msg);
+	}
+
+	public static ApiError newUnknownError(String msg) {
+		return new ApiError(CODE_UNKNOWN_ERROR, msg);
 	}
 
 	public int code() {
